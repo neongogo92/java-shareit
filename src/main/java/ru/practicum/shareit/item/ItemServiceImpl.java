6ba51c6c8194ba2818ec88ced5282d7bd9a3a57b
Anton P <anton.neongogo@yandex.ru>
2024-01-11 12:41:10 +0300

    изменил метод findUserIfExists();

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -11,7 +11,6 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemShortDto;
 import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
