cc32feb3cfe1871a40eec9b832a119aac4beafe6
neongogo92 <anton.neongogo@yandex.ru>
2024-01-16 00:15:22 +0300

    Разбил на 2 сервиса

--- server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -22,7 +22,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = ItemController.class)
 public class ItemControllerTest {
